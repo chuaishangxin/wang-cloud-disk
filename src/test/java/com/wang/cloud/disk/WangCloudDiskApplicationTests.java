1fe04df907bd1d7d9aad1e27119ec57b1a0a113a
wangpengqiang <969732194@qq.com>
2019-12-01 14:49:35 +0800

    first commit

---
+++ src/test/java/com/wang/cloud/disk/WangCloudDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.cloud.disk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangCloudDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
